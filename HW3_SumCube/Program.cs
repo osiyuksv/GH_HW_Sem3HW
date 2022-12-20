3688025061c1459119478430065d2ed403e84ab7
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 20:53:17 +0300

    HW3_SumCube

---
+++ HW3_SumCube/Program.cs
@@ -0,0 +1,11 @@
+﻿// Для числа N вывести 
+// таблицу кубов от 1 до N
+Console.WriteLine("Введите число N>0");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(1); 
+
+for(int i = 2; i <= n; i++)
+{
+    Console.Write(", " + i * i* i); 
+}
